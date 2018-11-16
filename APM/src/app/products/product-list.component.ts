b2cef544f69b8f3401e1d0c3a373fd0a90526558
richglitch <ricardo.pro@outlook.com>
2018-11-15 21:20:47 -0600

    Filters

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,12 +6,29 @@ import { IProduct } from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent 
+implements OnInit{
+  
+  ngOnInit(): void {
+    console.log('In OnInit');
+  }
+
   title: string = 'Product list';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  //listFilter: string = 'cart';
+  _listFilter: string;
+  get listFilter():string{
+    return this._listFilter;
+  }
+
+  set listFilter(value: string){
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
 
   products: IProduct[] = [
     {
@@ -34,6 +51,15 @@ export class ProductListComponent {
       "starRating": 4.2,
       "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }];
+    constructor(){
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart';
+    }
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1); //creates a new array with items matching with filterBy string
+    }
 
     toggleImage():void{
       this.showImage = !this.showImage;
